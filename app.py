49e25158bfc293a2f3ca6ecd9dc8bd6fc751f1ed
Daria <jonshilin.gg@gmail.com>
2024-05-13 22:15:03 +0300

    changed the method of receiving xml

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import xml.etree.ElementTree as ET
+import requests
+from datetime import datetime
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField, IntegerField
@@ -10,14 +12,16 @@ app = Flask(__name__)
 load_dotenv('.env')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-print("Loaded SECRET_KEY:", app.config['SECRET_KEY'])
-
 if app.config['SECRET_KEY'] is None:
     raise ValueError("No SECRET_KEY set for Flask application")
 
 def parse_xml_currency_data():
-    tree = ET.parse('currencies.xml')
-    root = tree.getroot()
+    today_date = datetime.now().strftime("%d/%m/%Y")
+    url = f"https://www.cbr.ru/scripts/XML_daily.asp?date_req={today_date}"
+
+    response = requests.get(url)
+    root = ET.fromstring(response.content)
+
     currencies = {}
     for valute in root.findall('Valute'):
         char_code = valute.find('CharCode').text
@@ -26,6 +30,7 @@ def parse_xml_currency_data():
         currencies[char_code] = (value / nominal, valute.find('Name').text)
     return currencies
 
+
 def convert_currency(amount: int, currency1: str, currency2: str):
     currencies = parse_xml_currency_data()
     rate1 = currencies[currency1][0]
@@ -33,23 +38,29 @@ def convert_currency(amount: int, currency1: str, currency2: str):
     converted_amount = (amount * rate1) / rate2
     return converted_amount
 
+
 def get_currencies():
     currencies = parse_xml_currency_data()
     currency_choices = [(code, f"{code} - {name}") for code, (rate, name) in currencies.items()]
     return currency_choices
 
-class CurrencyConvertForm(FlaskForm):
-    currencies = get_currencies()
+
+class CurrencyConvertFunaskForm(FlaskForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        currency_choices = get_currencies()
+        self.currency1.choices = currency_choices
+        self.currency2.choices = currency_choices
+
     amount = IntegerField("Amount", validators=[InputRequired()])
-    currency1 = SelectField("Currency From", choices=currencies, validators=[InputRequired()])
-    currency2 = SelectField("Currency To", choices=currencies, validators=[InputRequired()])
+    currency1 = SelectField("Currency From", validators=[InputRequired()])
+    currency2 = SelectField("Currency To", validators=[InputRequired()])
     submit = SubmitField("Convert")
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    form = CurrencyConvertForm()
-    form.currency1.choices = get_currencies()
-    form.currency2.choices = get_currencies()
+    form = CurrencyConvertFunaskForm()
     if form.validate_on_submit():
         amount = form.amount.data
         currency1 = form.currency1.data
@@ -69,6 +80,7 @@ def home():
         return render_template('index.html', form=form, converted_data=converted_data)
 
     return render_template('index.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
